e0edff7b4984ebe5423f1226c1ee6b6064666268
Bappaditya Sinha <bappadityasinha.92@gmail.com>
2024-08-28 21:34:01 +0530

    adding async code

---
+++ async.js
@@ -0,0 +1,12 @@
+// console.log("Hello");
+// setTimeout(()=>{
+//     console.log("hello from setTimeout")
+// }, 5000);
+// console.log("Javascript");
+// console.log("typescript")
+console.log('start');
+setTimeout(()=>{
+  console.log("i am from timeout");
+    
+},0)
+console.log('end');
